b6c3de6961a5b65938d1c92b185c49d0ee21b1c1
James Bradford <2324024b@student.gla.ac.uk>
2019-03-05 18:13:08 +0000

    Fix (Mostly) Population Script
    Population script now populates questions and users, but does not create new answers,
    as answers are actually linked to UserProfiles not Users, will need to add UserProfiles
    into pop script before this can be done. Same for Questions needing User Profiles.
    
    Also improved file formatting.
    
    This time I actually done it...

--- populate_ask_students.py
+++ populate_ask_students.py
@@ -5,63 +5,122 @@ import django
 
 django.setup()
 
-from ask_students.models import Category, Question
+from ask_students.models import Category, Question, User, Answer
 
 
 def populate():
 
-    #Changed from list of dictionary to nested dictionary
-    users = {'YellowPony123' : {'username': 'YellowPony123', 'first_name':'Abe', 'second_name':'MacCabe', 'password':'pigsdontfly'},
-             'AngryTelephonePole87' : {'username':'AngryTelephonePole87', 'first_name':'Belle' 'second_name':'MacKell', 'password':'BigF4TW1ndowL3dge'},
-             'ooeeooahahtingtang' : {'username':'ooeeooahahtingtang', 'first_name':'Charlie' 'second_name':'MacFarley', 'password':'wallawallabingbang'},
-             'DampSeatOnTheBus' : {'username':'DampSeatOnTheBus', 'first_name':'Doris' 'second_name':'MacBoris', 'password':'s4ndp4p3r'},
-             'SweetEdna' : {'username':'SweetEdna', 'first_name':'Edna' 'second_name':'MacScedna', 'password':'Edna'},
-             'SeriousFred' : {'username':'SeriousFred', 'first_name':'Fred' 'second_name':'MacSuede', 'password':'18gsa35ds68'},
-             'GustySeagull1942' : {'username':'GustySeagull1942', 'first_name':'Gill' 'second_name':'MacMill', 'password':'TROONBEACH'},
-             'HealthyLivingForAHealthyBeing' : {'username': 'HealthyLivingForAHealthyBeing', 'first_name':'Harriot', 'last_name':'MacTarrot', 'password':'carrot'},
-             'AintGotNoMoney' : {'username': 'AintGotNoMoney', 'first_name': 'Isabelle', 'last_name' : 'MacDinnerBell', 'password':'hahaha'},
+    # Changed from list of dictionary to nested dictionary
+    users = {'YellowPony123': {'username': 'YellowPony123', 'first_name': 'Abe', 'last_name': 'MacCabe',
+                               'password': 'pigsdontfly'},
+             'AngryTelephonePole87': {'username': 'AngryTelephonePole87', 'first_name': 'Belle', 'last_name': 'MacKell',
+                                      'password': 'BigF4TW1ndowL3dge'},
+             'ooeeooahahtingtang': {'username': 'ooeeooahahtingtang', 'first_name': 'Charlie', 'last_name': 'MacFarley',
+                                    'password': 'wallawallabingbang'},
+             'DampSeatOnTheBus': {'username': 'DampSeatOnTheBus', 'first_name': 'Doris', 'last_name': 'MacBoris',
+                                  'password': 's4ndp4p3r'},
+             'SweetEdna': {'username': 'SweetEdna', 'first_name': 'Edna', 'last_name': 'MacScedna',
+                           'password': 'Edna'},
+             'SeriousFred': {'username': 'SeriousFred', 'first_name': 'Fred', 'last_name': 'MacSuede',
+                             'password': '18gsa35ds68'},
+             'GustySeagull1942': {'username': 'GustySeagull1942', 'first_name':'Gill', 'last_name':'MacMill',
+                                  'password': 'TROONBEACH'},
+             'HealthyLivingForAHealthyBeing': {'username': 'HealthyLivingForAHealthyBeing', 'first_name':'Harriot',
+                                                'last_name': 'MacTarrot', 'password': 'carrot'},
+             'AintGotNoMoney': {'username': 'AintGotNoMoney', 'first_name': 'Isabelle',
+                                'last_name' : 'MacDinnerBell', 'password': 'hahaha'},
              }
-    for user in users.items:
+
+    for user in users.values():
         add_user(user['username'], user['first_name'], user['last_name'], user['password'])
 
-    
-    
-    categories = {'General': {'description': 'Ask general questions about any topic you like',
-                              'approved': True,
-                              'questions': [
-                                  {'name': "Who let the dogs out?", "description": "who?", "views": 10, "answers": [
-                                      {'answer':'who?! who?! who?!', 'likes':5, 'dislikes':3, 'posted': None, 'edited': None, 'user': 'YellowPony123'},
-                                      {'answer':'Baha Men', 'likes':10, 'dislikes':0, 'posted': None, 'edited': None, 'user': 'AngryTelephonePole87'}
-                                      ]},
-                                  {'name': "What's brown and sticky?", "description": "serious answers only please.", "views": 30, "answers": [
-                                      {'answer':'A brown sticker', 'likes':4, 'dislikes':20, 'posted': None, 'edited': None, 'user': 'ooeeooahahtingtang'},
-                                      {'answer':'A stick', 'likes':27, 'dislikes':2, 'posted': None, 'edited': None, 'user': 'DampSeatOnTheBus' },
-                                      {'answer':'How do I upload pictures to Facebook', 'likes':0, 'dislikes': 23, 'posted': None, 'edited': None, 'user' : 'SweetEdna'}
-                              ]},
-                  'Computing': {'description': 'Ask questions about Computing',
-                                'approved': True,
-                                'questions': [
-                                    {'name': "What is the first index in an array", "description": "I am trying to learn but noone is taking this question seriously", "views": 120,"answers": [
-                                      {'answer':'0', 'likes': 70, 'dislikes':15, 'posted': None, 'edited': None, 'user': 'SeriousFred'},
-                                      {'answer':'1', 'likes':3, 'dislikes':85, 'posted': None, 'edited': None, 'user': 'GustySeagull1942'}]
-                                    {'name': "How to fix wifi not working", "description": "how to fix wifi not working", "views": 750, 'answers': [
-                                      {'answer':'turn it off and turn it back on again', 'likes': 433, 'dislikes':48, 'posted': None, 'edited': None, 'user': 'YellowPony123'},
-                                      {'answer':'How to fix wifi not working', 'likes':3, 'dislikes':20, 'posted': None, 'edited': None, 'user': 'SweetEdna'},
-                                      {'answer':"This isn't really the right place to ask this question, try finding a better suited category or create one yourself.", 'likes':683, 'dislikes':24, 'posted': None,'edited': None, 'user':'SeriousFred'}
-                                ]},
-                  'Student Living General': {'description': 'General questions on Student Living',
-                                  'approved': True,
-                                  'questions': [
-                                      {'name': "Where is a good place to eat out in Glasgow", "description": "Looking for healthy organic food", "views": 654, 'answers': [
-                                        {'answer':'McDonalds', 'likes': 70, 'dislikes':61, 'posted': None, 'edited': None, 'user': 'HealthyLivingForAHealthyBeing'},
-                                        {'answer':'BlueLagoon', 'likes':321, 'dislikes':29, 'posted': None, 'edited': None, 'user': 'AngryTelephonePole87'},
-                                        ]},
-                                      {'name': "Best non-alcoholic drink for students", "description": "amazing!", "views": 12, 'answers': [
-                                        {'answer':'Water', 'likes': 3, 'dislikes':0, 'posted': None, 'edited': None, 'user': 'AintGotNoMoney'},
-                                        {'answer':'J20', 'likes':1, 'dislikes':1, 'posted': None, 'edited': None, 'user': 'SeriousFred'},
-                                          ]}
-                                  ]}
-                  }
+    categories = {
+        'General':
+            {'description': 'Ask general questions about any topic you like',
+             'approved': True,
+             'questions':
+                 [
+                     {'name': "Who let the dogs out?",
+                      "description": "who?",
+                      "views": 10,
+                      "answers":
+                         [
+                             {'answer': 'who?! who?! who?!', 'likes': 5, 'dislikes': 3,
+                              'posted': None, 'edited': None, 'user': 'YellowPony123'},
+                             {'answer': 'Baha Men', 'likes': 10, 'dislikes': 0,
+                              'posted': None, 'edited': None, 'user': 'AngryTelephonePole87'},
+                         ]
+                      },
+                     {'name': "What's brown and sticky?", "description": "serious answers only please.", "views": 30, "answers":
+                         [
+                             {'answer':'A brown sticker', 'likes':4, 'dislikes':20,
+                              'posted': None, 'edited': None, 'user': 'ooeeooahahtingtang'},
+                             {'answer':'A stick', 'likes':27, 'dislikes':2,
+                              'posted': None, 'edited': None, 'user': 'DampSeatOnTheBus' },
+                             {'answer':'How do I upload pictures to Facebook', 'likes':0, 'dislikes': 23,
+                              'posted': None, 'edited': None, 'user' : 'SweetEdna'},
+                         ]
+                      }
+                 ]
+             },
+
+        'Computing':
+            {'description': 'Ask questions about Computing',
+             'approved': True,
+             'questions':
+                 [
+                     {'name': "What is the first index in an array",
+                      "description": "I am trying to learn but no one is taking this question seriously",
+                      "views": 120,
+                      "answers":
+                          [
+                              {'answer':'0', 'likes': 70, 'dislikes':15, 'posted': None, 'edited': None, 'user': 'SeriousFred'},
+                              {'answer':'1', 'likes':3, 'dislikes': 85, 'posted': None, 'edited': None, 'user': 'GustySeagull1942'},
+                          ]
+                      },
+                     {'name': "How to fix wifi not working",
+                      "description": "how to fix wifi not working",
+                      "views": 750,
+                      'answers':
+                         [
+                             {'answer':'turn it off and turn it back on again', 'likes': 433, 'dislikes': 48, 'posted': None, 'edited': None, 'user': 'YellowPony123'},
+                             {'answer':'How to fix wifi not working', 'likes': 3, 'dislikes': 20, 'posted': None, 'edited': None, 'user': 'SweetEdna'},
+                             {'answer':"This isn't really the right place to ask this question, "
+                                       "try finding a better suited category or create one yourself.",
+                              'likes':683, 'dislikes':24, 'posted': None,
+                              'edited': None, 'user':'SeriousFred'}
+                         ]
+                      },
+                 ]
+             },
+
+        'Student Living General':
+            {'description': 'General questions on Student Living',
+             'approved': True,
+             'questions':
+                 [
+                     {'name': "Where is a good place to eat out in Glasgow",
+                      "description": "Looking for healthy organic food",
+                      "views": 654,
+                      'answers':
+                         [
+                             {'answer':'McDonalds', 'likes': 70, 'dislikes' : 61,
+                              'posted': None, 'edited': None, 'user': 'HealthyLivingForAHealthyBeing'},
+                             {'answer':'BlueLagoon', 'likes':321, 'dislikes': 29,
+                              'posted': None, 'edited': None, 'user': 'AngryTelephonePole87'},
+                         ]},
+                     {'name': "Best non-alcoholic drink for students",
+                      "description": "amazing!", "views": 12, 'answers':
+                          [
+                              {'answer':' Water', 'likes': 3, 'dislikes':0,
+                               'posted': None, 'edited': None, 'user': 'AintGotNoMoney'},
+                              {'answer': 'J20', 'likes': 1, 'dislikes': 1,
+                               'posted': None, 'edited': None, 'user': 'SeriousFred'},
+                          ]
+                      }
+                 ]
+             }
+    }
 
     for cat, cat_data in categories.items():
         c = add_category(cat, cat_data['description'], cat_data['approved'])
@@ -71,10 +130,9 @@ def populate():
         for question in cat_data['questions']:
             add_question(question['name'], question['description'], question['views'], c)
             i += 1
-            for answer in question['answers']:
-                add_answer(answer['answer'], answer['likes'], answer['dislikes'], answer['posted'], answer['edited'], answer['user'])
-
-                                
+            # for answer in question['answers']:
+            #     add_answer(answer['answer'], answer['likes'], answer['dislikes'], answer['posted'],
+            #                answer['edited'], answer['user'])
 
         print("  Adding {0} questions to {1}...".format(str(i), str(c)))
 
@@ -90,16 +148,20 @@ def add_question(name, description, views, cat):
     q.save()
     return q
 
+
 def add_user(username, first_name, last_name, password):
     u = User.objects.get_or_create(username=username, first_name=first_name, last_name=last_name, password=password)[0]
     u.save()
     return u
 
+
 def add_answer(answer, likes, dislikes, posted, edited, username):
-    user=User.objects.get(username=username)
-    a = Answer.objects.get_or_create(text=answer, likes=likes, dislikes=dislikes, posted=posted, edited=edited, user=user)
+    user = User.objects.get(username=username)
+    a = Answer.objects.get_or_create(text=answer, likes=likes, dislikes=dislikes, posted=posted,
+                                     edited=edited, user=user)
     a.save()
     return a
+
 
 if __name__ == '__main__':
     print("Running ask_students population script...")
