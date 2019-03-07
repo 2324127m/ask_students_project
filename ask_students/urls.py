fec5f3a4b05a737ba10dcdf8b2fe443b32ddb2e1
Matthew Wilson <2251532W@student.gla.ac.uk>
2019-03-07 15:35:50 +0000

    Mapped request category view

--- ask_students/urls.py
+++ ask_students/urls.py
@@ -5,9 +5,10 @@ from ask_students import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^register_profile/', views.register_profile, name="register_profile"),
+    url(r'^register_profile/$', views.register_profile, name="register_profile"),
     url(r'^my_profile/$', views.my_profile, name="my_profile"),
     url(r'^profile/(?P<username>[\w\-]+)/$', views.profile, name="profile"),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/(?P<question_id>[1-9]\d*)/$', views.show_question, name='show_question'),
+    url(r'^request_category/$', views.request_category, name="request_category"),
 ]
