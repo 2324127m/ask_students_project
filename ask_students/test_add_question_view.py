cbd44f2573b0c74ce4f804f602e2fd63e9a0590d
Jasmine Casey <2325421C@student.gla.ac.uk>
2019-03-18 12:18:09 +0000

    Where ma models at

--- ask_students/test_add_question_view.py
+++ ask_students/test_add_question_view.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 
 
 def test_fixture1(category):
-    new_question = Question(name="Q1 has 90 views", category=category, views=90, posted=timezone.now() - timedelta(minutes=8))
+    new_question = Question(name="Q1 has 90 views", category=category, views=90, posted=timezone.now() - timedelta(minutes=8), anonymous = True)
 
     return new_question
 
@@ -17,13 +17,40 @@ class AddQuestionViewTests(TestCase):
         pass
         
     def TestQuestionFields(self):
-        pass
+        #Create a new category
+        new_category = Category(name = "Test Category 1")
+        new_category.save()
+        
+        #Create and save a question for that category
+        question = test_fixture1(new_category)
+        question.save()
+        
+        self.assertEqual('')
 
     def TestNonExistantQuestionReturnsNone(self):
-        pass
+        # Create a new category
+        new_category = Category(name="Test Category 1")
+        new_category.save()
+
+        # Get a repsonse when the question does not exist
+        response = self.client.get(reverse('add_question', kwargs={'category_name_slug': new_category.slug, 'question_id': 4321}))
+
+        self.assertEqual(response.context['question'], None)
 
     def TestAnonymousWorks(self):
-        pass
+        #Create a new category
+        new_category = Category(name = "Test Category 1")
+        new_category.save()
+        
+        #Create and save a question for that category
+        question = test_fixture1(new_category)
+        question.save()
 
     def TestQuestionPostWithoutFile(self):
-        pass
+        #Create a new category
+        new_category = Category(name = "Test Category 1")
+        new_category.save()
+        
+        response = self.client.get(reverse('add_question', kwargs={'category_name_slug': new_category.slug, 'question_id': 4321}))
+
+        self.assertEqual(response.context['question'], None)
