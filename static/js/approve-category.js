d543265541ddada7a1857e383d3a609c38cc9aba
Mohammad Majid <2324127m@student.gla.ac.uk>
2019-03-21 17:16:16 +0000

    JS changes, approve category COMPLETE 99%

---
+++ static/js/approve-category.js
@@ -0,0 +1,5 @@
+$(function() {
+    $('.approve-category').click(function() {
+        return window.confirm("Are you sure you want to approve this category? The description you submit here will be the PUBLIC description!");
+    });
+});
