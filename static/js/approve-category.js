601b498587dfe19ca38aeba5ba3db57f8731c254
Mohammad Majid <2324127m@student.gla.ac.uk>
2019-03-22 14:18:20 +0000

    Decline category fixed

--- static/js/approve-category.js
+++ static/js/approve-category.js
@@ -1,13 +1,12 @@
-
-function deleteCategoryRequest() {
-    return window.confirm("Are you 100% sure you want to delete this request? This cannot be undone!");
-}
-
 $(document).ready(function() {
     $('.approve-category').click(function() {
         return window.confirm("Are you sure you want to approve this category? The description you submit here will be the PUBLIC description!");
-    });,
-    $('.delete-category-request').click(function() {
+    });
+});
+
+
+$(document).ready(function() {
+    $('.delete_category_request').click(function() {
         return window.confirm("Are you 100% sure you want to delete this request? This cannot be undone!");
     });
 });
