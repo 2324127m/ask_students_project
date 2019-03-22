acb47ac6cf3c1327026d0f71c4b8296b8e17bb48
Mohammad Majid <2324127m@student.gla.ac.uk>
2019-03-22 15:13:31 +0000

    Template tags directory

--- ask_students/templatetags/ask_students_templatetags.py
+++ ask_students/templatetags/ask_students_templatetags.py
@@ -4,18 +4,18 @@ from ask_students.models import Category, Question, Answer
 register = template.Library()
 
 
-@register.inclusion_tag('ask_students/category_list.html')
+@register.inclusion_tag('ask_students/template_tags/category_list.html')
 def get_category_list():
     return {'categories': Category.objects.all()}
 
 
-@register.inclusion_tag('ask_students/latest_questions.html')
+@register.inclusion_tag('ask_students/template_tags/latest_questions.html')
 # def get_latest_questions(question=None):
 def get_latest_questions():
     return {'latest_questions': Question.objects.order_by('-posted')[:10]}
 
 
-@register.inclusion_tag('ask_students/question_card.html')
+@register.inclusion_tag('ask_students/template_tags/question_card.html')
 def show_question(question):
     if not isinstance(question, Question):
         return None
@@ -23,15 +23,7 @@ def show_question(question):
         return {'question': question}
 
 
-@register.inclusion_tag('ask_students/full_question_card.html')
-def show_full_question(question):
-    if not isinstance(question, Question):
-        return None
-    else:
-        return {'question': question}
-
-
-@register.inclusion_tag('ask_students/answer_card.html')
+@register.inclusion_tag('ask_students/template_tags/answer_card.html')
 def show_answer(answer, question, liked, disliked, user):
     if not isinstance(answer, Answer):
         return None
@@ -39,7 +31,7 @@ def show_answer(answer, question, liked, disliked, user):
         return {'answer': answer, 'liked': liked, 'disliked': disliked, 'question': question, 'user': user}
 
 
-@register.inclusion_tag('ask_students/form_error_fields.html')
+@register.inclusion_tag('ask_students/template_tags/form_error_fields.html')
 def display_form_error_fields(form):
     return {'form': form}
 
